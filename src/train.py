1394e8f0f9211e0bcc7a1143513216c317ea5eb6
OtaTran <otatran241@gmail.com>
2024-09-13 12:58:57 +0700

    CD fix commit

--- src/train.py
+++ src/train.py
@@ -1,6 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 import pickle
+import os
 
 # Load dataset
 data = load_iris()
@@ -9,6 +10,9 @@ X, y = data.data, data.target
 # Train model
 model = RandomForestClassifier()
 model.fit(X, y)
+
+if not os.path.exists('models'):
+    os.makedirs('models')
 
 # Save the model to disk
 with open('models/model.pkl', 'wb') as f:
