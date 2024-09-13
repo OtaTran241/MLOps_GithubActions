5123b8943ee0ec75d71f90ff5b43164ee10235fd
OtaTran <otatran241@gmail.com>
2024-09-13 13:30:50 +0700

    fix CML commit

--- src/test.py
+++ src/test.py
@@ -5,7 +5,6 @@ import os
 
 class TestModel(unittest.TestCase):
     def setUp(self):
-        # Cập nhật đường dẫn đến model.pkl
         model_path = os.path.join(os.path.dirname(__file__), '../models/model.pkl')
         with open(model_path, 'rb') as f:
             self.model = pickle.load(f)
@@ -13,6 +12,12 @@ class TestModel(unittest.TestCase):
     def test_prediction(self):
         test_data = np.array([5.1, 3.5, 1.4, 0.2])
         prediction = self.model.predict(test_data.reshape(1, -1))
+        
+        with open("single_test.txt", 'w') as outfile:
+            outfile.write("Single test Results:\n")
+            outfile.write(f"Input: {test_data.tolist()}\n")
+            outfile.write(f"Prediction: {int(prediction[0])}\n")
+        
         self.assertIn(prediction[0], [0, 1, 2])
 
 if __name__ == '__main__':
--- src/train.py
+++ src/train.py
@@ -1,5 +1,10 @@
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd 
 import pickle
 import os
 
@@ -7,9 +12,14 @@ import os
 data = load_iris()
 X, y = data.data, data.target
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 # Train model
 model = RandomForestClassifier()
-model.fit(X, y)
+model.fit(X_train, y_train)
+
+train_score = model.score(X_train, y_train) * 100
+test_score = model.score(X_test, y_test) * 100
 
 if not os.path.exists('models'):
     os.makedirs('models')
@@ -17,5 +27,32 @@ if not os.path.exists('models'):
 # Save the model to disk
 with open('models/model.pkl', 'wb') as f:
     pickle.dump(model, f)
+
+with open("metrics.txt", 'w') as outfile:
+    outfile.write("Metric of Results:\n")
+    outfile.write("Training variance explained: %2.1f%%\n" % train_score)
+    outfile.write("Test variance explained: %2.1f%%\n" % test_score)
+
+# image formatting
+axis_fs = 18 
+title_fs = 22 
+
+y_pred = model.predict(X_test) + np.random.normal(0,0.25,len(y_test))
+y_jitter = y_test + np.random.normal(0,0.25,len(y_test))
+res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true","pred"])
+
+ax = sns.scatterplot(x="true", y="pred",data=res_df)
+ax.set_aspect('equal')
+ax.set_xlabel('True wine quality',fontsize = axis_fs) 
+ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
+ax.set_title('Residuals', fontsize = title_fs)
+
+# Make it pretty- square aspect ratio
+ax.plot([1, 10], [1, 10], 'black', linewidth=1)
+plt.ylim((2.5,8.5))
+plt.xlim((2.5,8.5))
+
+plt.tight_layout()
+plt.savefig("residuals.png",dpi=120) 
 
 print("Model training completed!")
